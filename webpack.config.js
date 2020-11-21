c2d66044282801b4efd13b9ddf7de9021711f960
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-11-21 17:33:01 -0500

    del host in develop script

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,12 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    // publicPath: '/',
   },
-  // devServer: {
-  //   historyApiFallback: true,
-  // },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
